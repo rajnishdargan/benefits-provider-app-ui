4508d2c000a6fe735159a0e637bf87c54ff46926
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-05-09 15:45:54 +0530

    Fix-Removed uba-ui

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import "dotenv/config";
 
 export default defineConfig({
   base: "",
-  server: {
-    proxy: {
-      "/application": {
-        target: process.env.VITE_APP_PROXY_API,
-        changeOrigin: true,
-        secure: false,
-      },
-      "/benefits": {
-        target: process.env.VITE_APP_PROXY_API,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/application": {
+  //       target: process.env.VITE_APP_PROXY_API,
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     "/benefits": {
+  //       target: process.env.VITE_APP_PROXY_API,
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
