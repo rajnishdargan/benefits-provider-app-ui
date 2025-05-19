d17ebff0fc9fe1337c0d4124f8656afab752bc66
Vidya Hanumant Khandekar <vidya.khandekar@tekditechnologies.com>
2025-05-19 18:55:25 +0530

    Add Options for export csv (#95)
    * Button label change Accept->Approve
    
    * Add options in csv

--- src/components/DownloadCSV.tsx
+++ src/components/DownloadCSV.tsx
@@ -22,6 +22,9 @@ interface DownloadCSVProps {
 const options = [
   { label: "SBI to SBI", value: "sbiToSbi" },
   { label: "SBI to Other Bank", value: "sbiToOtherBanks" },
+  { label: "All Application Data Fields", value: "allApplicationDataFields" },
+  { label: "De Duplication NSP", value: "de_duplication_nsp" },
+  { label: "De Duplication Alimco", value: "de_duplication_alimco" },
 ];
 
 const DownloadCSV: React.FC<DownloadCSVProps> = ({
